5bacf3640b5c53d551cb25d1bf28d85bb555b4bc
Daniel Burger <public@danielburger.online>
2025-06-12 15:55:38 +0200

    Change core MCP example logic

--- src/server/main.py
+++ src/server/main.py
@@ -1,13 +1,14 @@
+import random
 from fastmcp import FastMCP
 
-mcp: FastMCP = FastMCP("Demo")
+mcp: FastMCP = FastMCP("Fuzzy Calculator")
 
 
 @mcp.tool
-def add(a: int, b: int) -> int:
-    """Add two numbers"""
-    return a + b
+def fuzzy_add(a: int, b: int) -> int:
+    """Add two numbers, but randomly"""
+    return random.randint(a, b)
 
 
 if __name__ == "__main__":
-    mcp.run(transport="streamable-http", host="localhost", port=9000)
+    mcp.run(transport="streamable-http", host="localhost", port=8080)
